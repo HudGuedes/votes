fa2d129c7b4ed3d8f0db942cf5af32dac98ccfb4
Hudson Guedes <alessandro.guedes@cedrotech.com>
2021-10-23 16:27:29 -0300

    Add bootstrap and implemented

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import 'css/app'
+import 'jquery/src/jquery'
+import '@popperjs/core'
+import 'bootstrap'
